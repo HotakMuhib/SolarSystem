7ee200126ac8880693464c0cc8efba6bf9ab7c70
HotakMuhib <123457don@gmail.com>
2022-05-15 14:19:29 -0400

    Solar system basic ready

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,18 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        SolarSystem factorX = new SolarSystem();
+        factorX.sun="1";
+        factorX.moon="2";
+        factorX.planet="4";
+        factorX.stars="1000";
+    }
+}
+
+
+//Branching
+//Scenario:
+//create a basic structure of a solar system and deploy as soon as possible.
